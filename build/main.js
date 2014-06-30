dfef07b953286cb0d5cd9c63a0b968f28e7e8c80
Neil <neil-lu@graduate.uwa.edu.au>
2014-06-30 22:23:24 +0800

    users can randomly choose several lotteries

--- build/main.js
+++ build/main.js
@@ -1,10 +1,18 @@
 (function() {
-  var Lottery, lottery;
+  var Lottery, i, lottery, _i, _len, _ref;
 
   Lottery = require('./lottery');
 
   lottery = new Lottery();
 
-  lottery.bang();
+  if (process.argv.length >= 3) {
+    _ref = [0, process.argv[2]];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      i = _ref[_i];
+      lottery.bang();
+    }
+  } else {
+    lottery.bang();
+  }
 
 }).call(this);
--- src/main.coffee
+++ src/main.coffee
@@ -2,4 +2,7 @@ Lottery = require './lottery'
 
 lottery = new Lottery()
 
-lottery.bang()
+if process.argv.length >= 3
+  lottery.bang() for i in [0, process.argv[2]]
+else
+  lottery.bang()
